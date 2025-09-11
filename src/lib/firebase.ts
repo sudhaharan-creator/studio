85e285ce95ddba9b16f4cb21f20314fe4090d3c3
Sudhaharan H IPM 2021-26 Batch <i21sudhaharanh@iimidr.ac.in>
2025-09-11 18:53:15 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -8,12 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  "projectId": "studio-9215288788-3958f",
+  "appId": "1:96996764351:web:4c9bae50f5aad18c85740a",
+  "storageBucket": "studio-9215288788-3958f.firebasestorage.app",
+  "apiKey": "AIzaSyA-sdmOHfoGYfqXDUu1VBWcGiYyYCX3xXI",
+  "authDomain": "studio-9215288788-3958f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "96996764351"
 };
 
 // Initialize Firebase
