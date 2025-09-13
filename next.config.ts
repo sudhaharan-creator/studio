f14fe74b2ef75ffa76c6fc0a7f79786c2b5acdc6
Sudhaharan H IPM 2021-26 Batch <i21sudhaharanh@iimidr.ac.in>
2025-09-13 07:29:14 +0000

    yes

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,13 @@
 import type {NextConfig} from 'next';
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  // add your own strategies to the existing ones
+  // Your other PWA configs...
+});
+
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -32,4 +41,3 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
